504007abfd477f7e9c10d926f89fa533c17b49c9
pointsqatar <pointsqatar@outlook.com>
2016-11-07 15:50:43 +0300

    working on SMPT
    not done

--- AlRadi/Controllers/HomeController.cs
+++ AlRadi/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
 
 namespace AlRadi.Controllers
 {
@@ -13,5 +15,36 @@ namespace AlRadi.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult SendMail(FromMail mail)
+        {
+            try
+            {
+                MailAddress mailFrom = new MailAddress("info@alradi-contracting.com", mail.Name);
+                MailAddress mailTo = new MailAddress("info@pointsqatar.com", "Alradi");
+                MailMessage message = new MailMessage(mailFrom, mailTo);
+                message.Body = mail.Message;
+                message.Subject = mail.Subject;
+                SmtpClient client = new SmtpClient();
+                client.Host = "smtpout.europe.secureserver.net ";
+                client.Port = 465;
+                client.Credentials = new NetworkCredential("info@alradi-contracting.com", "Admin@2016");
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.EnableSsl = true;
+                client.Send(message);
+                return Content("Success");
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.Message + ex.InnerException + ex.StackTrace);
+            }
+        }
+    }
+    public class FromMail
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
     }
 }
