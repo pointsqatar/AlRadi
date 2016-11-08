8028dfaf9cd2bcbabba6926bc3cff68aa79a229c
pointsqatar <pointsqatar@outlook.com>
2016-11-08 15:48:40 +0300

    AlRadi- completed

--- AlRadi/Controllers/HomeController.cs
+++ AlRadi/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Net;
-using System.Net.Mail;
+using System.Web.Mail;
 
 namespace AlRadi.Controllers
 {
@@ -20,18 +20,19 @@ namespace AlRadi.Controllers
         {
             try
             {
-                MailAddress mailFrom = new MailAddress("info@alradi-contracting.com", mail.Name);
-                MailAddress mailTo = new MailAddress("info@pointsqatar.com", "Alradi");
-                MailMessage message = new MailMessage(mailFrom, mailTo);
+                //MailAddress mailFrom = new MailAddress("admin@alradi-contracting.com", mail.Name);
+                //MailAddress mailTo = new MailAddress("info@pointsqatar.com", "Alradi");
+                System.Web.Mail.MailMessage message = new System.Web.Mail.MailMessage();
+                message.From = "admin@alradi-contracting.com";
+                message.To = "info@pointsqatar.com";
                 message.Body = mail.Message;
                 message.Subject = mail.Subject;
-                SmtpClient client = new SmtpClient();
-                client.Host = "smtpout.europe.secureserver.net";
-                client.Port = 465;
-                client.Credentials = new NetworkCredential("info@alradi-contracting.com", "Admin@2016");
-                client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                client.EnableSsl = true;
-                client.Send(message);
+                SmtpMail.SmtpServer = "relay-hosting.secureserver.net";
+                SmtpMail.Send(message);
+
+
+                //client.Host = "smtpout.secureserver.net";
+                //client.Send(message);
                 return Content("Success");
             }
             catch (Exception ex)
